16d5368336e1d6e05e9a1399748abf628a0f6024
AinTEAsports <benkirat.khalil@gmail.com>
2022-04-21 14:33:37 +0200

    Removed unused imports

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import json
-import random
-import calendar
 
 from flask import Flask
 
@@ -12,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/get-ascii/<style>/<character_width>/<character_height>/<text>", methods=['GET'])
 def getCalendar(style : str, character_width : str, character_height : str, text : str):
-    return wG.generateAscii(style=style, characterWidth=character_width, characterHeight=character_height, text=text)
+    return json.dumps({'ascii' : wG.generateAscii(style=style, characterWidth=character_width, characterHeight=character_height, text=text)})
 
 
 app.run(debug=True)
