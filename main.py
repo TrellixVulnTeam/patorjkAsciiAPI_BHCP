ced3f34d1a3cdcef1b31c52bf8d399cbf758abce
AinTEAsports <benkirat.khalil@gmail.com>
2022-04-21 18:51:56 +0200

    Changed main function name and changed return to simple string containing ascii art

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 
 @app.route("/get-ascii/<style>/<character_width>/<character_height>/<text>", methods=['GET'])
-def getCalendar(style : str, character_width : str, character_height : str, text : str):
-    return json.dumps({'ascii' : wG.generateAscii(style=style, characterWidth=character_width, characterHeight=character_height, text=text)})
+def getAscii(style : str, character_width : str, character_height : str, text : str):
+    return wG.generateAscii(style=style, characterWidth=character_width, characterHeight=character_height, text=text)
 
 
 app.run(debug=True)
