cd14ed2e30a6a9a2600a9b3eeb414ea353e4729a
AinTEAsports <benkirat.khalil@gmail.com>
2022-04-21 19:18:48 +0200

    Added some documentation on main.py get function

--- main.py
+++ main.py
@@ -9,7 +9,19 @@ app = Flask(__name__)
 
 
 @app.route("/get-ascii/<style>/<character_width>/<character_height>/<text>", methods=['GET'])
-def getAscii(style : str, character_width : str, character_height : str, text : str):
+def getAscii(style : str, character_width : str, character_height : str, text : str) -> str() :
+    """Function that permite to get ASCII from website
+
+    Args:
+        textStyle (str): the text style you want
+        characterWidth (str): the character width
+        characterHeight (str): the character height
+        text (str): the text you want to convert to ASCII art
+
+    Returns:
+        str: ASCII art got from the website
+    """
+    
     return wG.generateAscii(style=style, characterWidth=character_width, characterHeight=character_height, text=text)
 
 
